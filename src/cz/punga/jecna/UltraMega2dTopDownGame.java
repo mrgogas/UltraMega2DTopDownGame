340e776d7a4ff1ed206bd333216a02b72c4686c5
mrgogas <Mr.gogas@seznam.cz>
2014-07-01 23:03:18 +0200

    kk

---
+++ src/cz/punga/jecna/UltraMega2dTopDownGame.java
@@ -0,0 +1,19 @@
+package cz.punga.jecna;
+
+import java.awt.Insets;
+
+import cz.punga.jecna.App;
+
+public class UltraMega2dTopDownGame{
+	public static void main(String args[]){
+		App app = App.app;
+	    app.initCanvas();
+	    app.setVisible(true);
+	    app.setTitle( "UltraMega2dTopDownGame");
+	    app.setResizable( false );
+	    Insets i = app.getInsets();
+	    app.setSize(App.WIDTH+i.left-2, App.HEIGHT+i.top-2);
+	    app.setLocation(100, 100);
+	    app.run();
+	}
+}
