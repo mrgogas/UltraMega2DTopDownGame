d910f944c63219666ab47b9a12e47bd5ebe712f2
mrgogas <Mr.gogas@seznam.cz>
2014-07-02 00:11:15 +0200

    Entity

--- src/cz/punga/jecna/entity/Entity.java
+++ src/cz/punga/jecna/entity/Entity.java
@@ -1,5 +1,31 @@
 package cz.punga.jecna.entity;
 
-public class Entity {
-int a;
+public abstract class Entity {
+protected String src;
+private int x;
+private int y;
+
+public Entity(int x, int y)
+{
+	this.x = x;
+	this.y = y;
+}
+public int getX()
+{
+	return x;
+}
+public void setX(int x)
+{
+	this.x = x;
+}
+public int getY()
+{
+	return y;
+}
+public void setY(int y)
+{
+	this.y = y;
+}
+public abstract void draw();
+public abstract void colide();
 }
---
+++ src/cz/punga/jecna/entity/EntityManager.java
@@ -0,0 +1,15 @@
+package cz.punga.jecna.entity;
+
+import java.util.ArrayList;
+
+public class EntityManager {
+private static ArrayList<Entity> ed = new ArrayList<Entity>();
+void add(Entity e)
+{
+	ed.add(e);
+}
+protected int w;
+protected int h;
+
+public ()
+}
