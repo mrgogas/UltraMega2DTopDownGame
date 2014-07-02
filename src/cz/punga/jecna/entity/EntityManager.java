8a967ea0c58f27d47720f43ca63463f1d0426aaa
Punga3 <firma.ondra@gmail.com>
2014-07-02 18:25:31 +0200

    Basic EntityManager stuff

--- src/cz/punga/jecna/entity/EntityManager.java
+++ src/cz/punga/jecna/entity/EntityManager.java
@@ -3,9 +3,15 @@ package cz.punga.jecna.entity;
 import java.util.ArrayList;
 
 public class EntityManager {
-private static ArrayList<Entity> ed = new ArrayList<Entity>();
-void add(Entity e)
-{
-	ed.add(e);
-}
+	private static ArrayList<Entity> entityList = new ArrayList<Entity>();
+	public static void add(Entity e)
+	{
+		entityList.add(e);
+	}
+	public static void now(){
+		ArrayList<Entity> collideList = new ArrayList<Entity>();
+		for(Entity en : entityList){
+			
+		}
+	}
 }
